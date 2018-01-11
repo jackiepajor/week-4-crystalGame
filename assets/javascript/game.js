3cb331fbf55c4e7106e958cd6423555ca5931e72
Jaclyn Pajor <jpajor@grad.bryant.edu>
2018-01-10 22:22:37 -0500

    edited files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,26 +2,28 @@
 var userTotalScore = 0;
 var wins = 0;
 var losses = 0;
+var min = 19;
+var max = 120;
 
-//Pick a random number between 19 and 120 and display it to the user
+//Pick a random number between 19(min) and 120(max) and display it to the user
 $(document).ready(function () {
-  var Random = Math.floor(Math.random() * 120 + 19)
-  $('#matchThisScore').text(Random);
+ var random = Math.floor(Math.random() * (max - min + 1)) + min;
+  $('#matchThisScore').text(random);
 
   //Pick a random number between 1 and 12 for each of the 4 crystals
-  var crystal1 = Math.floor(Math.random() * 12 + 1)
-  var num2 = Math.floor(Math.random() * 12 + 1)
-  var num3 = Math.floor(Math.random() * 12 + 1)
-  var num4 = Math.floor(Math.random() * 12 + 1)
+  var crystal1 = Math.floor(Math.random() * 11 + 1)
+  var crystal2 = Math.floor(Math.random() * 11 + 1)
+  var crystal3 = Math.floor(Math.random() * 11 + 1)
+  var crystal4 = Math.floor(Math.random() * 11 + 1)
 
-  //Alert and display wins
+  //Display wins, increment by 1 point per win, and then reset the game
   function winner() {
     alert("Congrats, you win! Click OK to play again!");
     wins++;
     $('#wins').text(wins);
     reset();
   }
-  //Alert and display losses
+  //Display losses, increment by 1 point per loss, and then reset the game
   function loser() {
     alert("Sorry, you lost! Click OK to play again!");
     losses++;
@@ -38,62 +40,62 @@ $(document).ready(function () {
    //Clear Crystal
   $('.clear').on('click', function () {
     userTotalScore = userTotalScore + crystal1;
-    console.log("New userTotalScore= " + userTotalScore);
+    console.log("new userTotalScore= " + userTotalScore);
     $('#totalScore').text(userTotalScore);
 
-    if (userTotalScore == Random) {
+    if (userTotalScore == random) {
       winner();
-    } else if (userTotalScore > Random) {
+    } else if (userTotalScore > random) {
       loser();
     }
   })
     //Rainbow Crystal
   $('.rainbow').on('click', function () {
-    userTotalScore = userTotalScore + num2;
-    console.log("New userTotalScore= " + userTotalScore);
+    userTotalScore = userTotalScore + crystal2;
+    console.log("new userTotalScore= " + userTotalScore);
     $('#totalScore').text(userTotalScore);
 
-    if (userTotalScore == Random) {
+    if (userTotalScore == random) {
       winner();
-    } else if (userTotalScore > Random) {
+    } else if (userTotalScore > random) {
       loser();
     }
   })
     //Purple Crystal
   $('.purple').on('click', function () {
-    userTotalScore = userTotalScore + num3;
-    console.log("New userTotalScore= " + userTotalScore);
+    userTotalScore = userTotalScore + crystal3;
+    console.log("new userTotalScore= " + userTotalScore);
     $('#totalScore').text(userTotalScore);
 
-    if (userTotalScore == Random) {
+    if (userTotalScore == random) {
       winner();
-    } else if (userTotalScore > Random) {
+    } else if (userTotalScore > random) {
       loser();
     }
   })
     //Pink Crystal
   $('.pink').on('click', function () {
-    userTotalScore = userTotalScore + num4;
-    console.log("New userTotalScore= " + userTotalScore);
+    userTotalScore = userTotalScore + crystal4;
+    console.log("new userTotalScore= " + userTotalScore);
     $('#totalScore').text(userTotalScore);
 
-    if (userTotalScore == Random) {
+    if (userTotalScore == random) {
       winner();
-    } else if (userTotalScore > Random) {
+    } else if (userTotalScore > random) {
       loser();
     }
   });
 });
 
-//Reset the game - Generate a new number for user to match, reassign crystal values, and clear the scoreboard
+//Reset the game - Generate a new number for user to match and reassign crystal values
 function reset() {
-  Random = Math.floor(Math.random() * 120 + 19);
+  random = Math.floor(Math.random() * 120 + 19);
   console.log(Random)
-  $('#matchThisScore').text(Random);
-  crystal1 = Math.floor(Math.random() * 12 + 1);
-  num2 = Math.floor(Math.random() * 12 + 1);
-  num3 = Math.floor(Math.random() * 12 + 1);
-  num4 = Math.floor(Math.random() * 12 + 1);
+  $('#matchThisScore').text(random);
+  crystal1 = Math.floor(Math.random() * 11 + 1);
+  crystal2 = Math.floor(Math.random() * 11 + 1);
+  crystal3 = Math.floor(Math.random() * 11 + 1);
+  crystal4 = Math.floor(Math.random() * 11 + 1);
   userTotalScore = 0;
   $('#totalScore').text(userTotalScore);
 }
